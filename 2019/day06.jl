df34da99a9729462e2150fc45072f2e44b4fb455
Eric Greveson <eric@greveson.co.uk>
2019-12-08 18:57:48 +0000

    Simplify day 6 code a bit (was unnecessarily complicated before)

--- 2019/day06.jl
+++ 2019/day06.jl
@@ -6,7 +6,10 @@ mutable struct Node
     parent::Union{Nothing, Node}
 end
 
-"""Build a tree from a list of [parent, child] pairs, assuming there is exactly one root node"""
+depth(node::Node) = isnothing(node.parent) ? 0 : (depth(node.parent) + 1)
+ancestors(node::Node) = isnothing(node.parent) ? [] : [node.parent; ancestors(node.parent)]
+
+"""Return Dict(name => Node) from list of [parent, child] pairs"""
 function build_tree(orbits)
     nodes = Dict()
     for (orbitee, orbiter) in orbits
@@ -15,29 +18,18 @@ function build_tree(orbits)
         orbiter_node.parent = orbitee_node
         push!(orbitee_node.children, orbiter_node)
     end
-    return first(filter(n -> isnothing(n.parent), collect(values(nodes))))
+    return nodes
 end
 
 # Part 1 - build orbit tree and compute depth of each node
 orbits = open(f -> split.(readlines(f), ")"), "day06_input.txt")
-tree = build_tree(orbits)
-total_orbits(node::Node, depth=0) = depth + sum([total_orbits.(node.children, depth + 1); 0])
-println("Total direct/indirect orbits: $(total_orbits(tree))")
+nodes = build_tree(orbits)
+println("Total direct/indirect orbits: $(sum(depth.(values(nodes))))")
 
 # Part 2 - minimum orbital transfers from YOU to SAN
-function get_ancestors(node::Node, name, ancestors=[])
-    new_ancestors = [ancestors; node]
-    if node.name == name return new_ancestors end
-    for child in node.children
-        child_result = get_ancestors(child, name, new_ancestors)
-        if !isnothing(child_result) return child_result end
-    end
-    return nothing
-end
-
-you_nodes = get_ancestors(tree, "YOU")
-santa_nodes = get_ancestors(tree, "SAN")
-non_common_nodes = symdiff(reverse(you_nodes), santa_nodes)
-print("Transfers: $(length(non_common_nodes) - 2)")
+you_nodes = ancestors(nodes["YOU"])
+santa_nodes = ancestors(nodes["SAN"])
+non_common_nodes = symdiff(you_nodes, santa_nodes)
+print("Transfers: $(length(non_common_nodes))")
 
 end
