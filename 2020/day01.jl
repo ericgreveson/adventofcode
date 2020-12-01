b326ab1837b0bd92539df73792efe5134df8e5a2
Eric Greveson <egreveson@brainomix.com>
2020-12-01 09:39:28 +0000

    Refinement using Combinatorics

--- 2020/day01.jl
+++ 2020/day01.jl
@@ -1,3 +1,5 @@
+using Combinatorics
+
 # Part 1
 # Load input
 expenses = open("day01_input.txt") do f
@@ -5,21 +7,15 @@ expenses = open("day01_input.txt") do f
 end
 
 # See which pair sum to 2020 and multiply them
-for (index, value1) in enumerate(expenses)
-    for value2 in expenses[index+1:end]
-        if value1 + value2 == 2020
-            println("Product of $value1 and $value2: $(value1 * value2)")
-        end
+for (value1, value2) in combinations(expenses, 2)
+    if value1 + value2 == 2020
+        println("Product of $value1 and $value2: $(value1 * value2)")
     end
 end
 
 # Part 2: which triplet satisfy these criteria?
-for (index1, value1) in enumerate(expenses)
-    for (index2, value2) in enumerate(expenses[index1+1:end])
-        for value3 in expenses[index1+index2+1:end]
-            if value1 + value2 + value3 == 2020
-                println("Product of $value1, $value2 and $value3: $(value1 * value2 * value3)")
-            end
-        end
+for values in combinations(expenses, 3)
+    if sum(values) == 2020
+        println("Product of $values: $(prod(values))")
     end
 end
