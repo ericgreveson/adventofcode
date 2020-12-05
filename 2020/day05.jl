5add87103a6e4e1dbefc21cca9d63074007f5416
Eric Greveson <egreveson@brainomix.com>
2020-12-05 10:48:14 +0000

    Less splatty

--- 2020/day05.jl
+++ 2020/day05.jl
@@ -1,7 +1,7 @@
 # Part 1 - parse passports
 passes = open(f -> strip.(readlines(f)), "day05_input.txt")
 seatids = Set(map(p -> parse(Int32, join([c ∈ ['F', 'L'] ? '0' : '1' for c in p]), base=2), passes))
-println("Part 1: Highest seat ID is $(max(seatids...))")
+println("Part 1: Highest seat ID is $(maximum(seatids))")
 
 # Part 2
 println("Part 2: seat ID is $(first([s+1 for s in seatids if (s+1) ∉ seatids && (s+2) ∈ seatids]))")
