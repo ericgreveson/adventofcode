3e971c39f0f92a2b7b39e1343001268600d821fd
Eric Greveson <egreveson@brainomix.com>
2020-12-15 21:20:31 +0000

    Finally did day 10 part 2

--- 2020/day10.jl
+++ 2020/day10.jl
@@ -5,18 +5,22 @@ ratings = sort(parse.(Int32, readlines("day10_input.txt")))
 diffs = vcat(ratings, [ratings[end]+3]) - vcat([0], ratings)
 println("Part 1: product = $(count(x -> x==1, diffs) * count(x -> x==3, diffs))")
 
-# Part 2 - note the diffs are ALL 1 or 3
-function minimal(ratings)
-    jolts = 0
-    remaining = []
-    for i in 1:(lastindex(ratings)-1)
-        if jolts + 3 < ratings[i+1]
-            # We need this rating, next one won't cut it
-            jolts = ratings[i]
-            push!(remaining, jolts)
+# Part 2
+valid(chain) = maximum([chain; chain[end]+3] - [chain[1]-3; chain]) <= 3
+innercombinations(chain) = combinations(chain[2:(lastindex(chain)-1)])
+swapguts(chain, inner) = [first(chain); inner; last(chain)]
+arrangements(chain) = count([[valid(swapguts(chain, c)) for c in innercombinations(chain)]; valid(swapguts(chain, []))])
+function subchains(chain)
+    results = []
+    current = [first(chain)]
+    for i in 2:lastindex(chain)
+        if last(current) + 3 == chain[i]
+            push!(results, current)
+            current = []
         end
+        push!(current, chain[i])
     end
-    [remaining; ratings[end]]
+    push!(results, current)
+    results
 end
-remaining = minimal(ratings)
-println("Part 2: arrangements = $(length(ratings) - length(remaining))")
+println("Part 2: arrangements = $(prod(arrangements.(subchains([0; ratings; ratings[end]+3]))))")
